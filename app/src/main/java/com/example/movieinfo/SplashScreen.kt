312a603c3f6f28dbcd1fb63bf47a13cfbe21307a
ahmedhosam283843 <ahmedhosam283843@gmail.com>
2022-07-17 12:22:58 +0200

    Added ViewPager

--- app/src/main/java/com/example/movieinfo/SplashScreen.kt
+++ app/src/main/java/com/example/movieinfo/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.movieinfo
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class SplashScreen : Fragment() {
 
@@ -12,6 +14,9 @@ class SplashScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_viewPagerFragment2)
+        }, 3000)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
--- app/src/main/java/com/example/movieinfo/onBoarding/ViewPagerFragment.kt
+++ app/src/main/java/com/example/movieinfo/onBoarding/ViewPagerFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.movieinfo.R
+import com.example.movieinfo.onBoarding.Screens.FirstFragment
+import com.example.movieinfo.onBoarding.Screens.SecondFragment
+import kotlinx.android.synthetic.main.fragment_view_pager.view.*
 
 
 class ViewPagerFragment : Fragment() {
@@ -18,11 +21,28 @@ class ViewPagerFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
-        val fragments = arrayListOf<Fragment>(
-
+        val fragmentList = arrayListOf<Fragment>(
+            FirstFragment(),
+            SecondFragment()
         )
+        val adapter = ViewPagerAdapter(fragmentList, requireActivity()
+            .supportFragmentManager, lifecycle)
+        view.viewPager.adapter = adapter
 
-        return view
+        view.bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.firstMenuItem -> {
+                    view.viewPager.currentItem = 0
+                    true
+                }
+                R.id.secondMenuItem -> {
+                    view.viewPager.currentItem = 1
+                    true
+                }
+                else -> false
+            }
+        }
+       return view
     }
 
 
