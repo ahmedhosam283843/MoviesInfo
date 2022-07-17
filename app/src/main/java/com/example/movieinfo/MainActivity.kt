00654708cf049df7f7526f1d6cf8ebbd44c80787
ahmedhosam283843 <ahmedhosam283843@gmail.com>
2022-07-17 15:31:03 +0200

    Added Recycler View

--- app/src/main/java/com/example/movieinfo/MainActivity.kt
+++ app/src/main/java/com/example/movieinfo/MainActivity.kt
@@ -4,12 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    val imgs = listOf<Int>(
-        androidx.customview.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.abc_ab_share_pack_mtrl_alpha,
-        androidx.viewpager.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_ic_go_search_api_material,
-        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.transition.R.drawable.abc_btn_check_to_on_mtrl_000, androidx.constraintlayout.widget.R.drawable.abc_spinner_textfield_background_material,
-        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_edit_text_material)
-    lateinit var s1 : Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/movieinfo/onBoarding/Screens/FirstScreen.kt
+++ app/src/main/java/com/example/movieinfo/onBoarding/Screens/FirstScreen.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movieinfo.R
+import com.example.movieinfo.search_ui.RecyclerAdapter
+import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_first.view.*
 
 
 class FirstFragment : Fragment() {
-
+    val imgs = listOf<Int>(
+        androidx.customview.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.abc_ab_share_pack_mtrl_alpha,
+        androidx.viewpager.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_ic_go_search_api_material,
+        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.transition.R.drawable.abc_btn_check_to_on_mtrl_000, androidx.constraintlayout.widget.R.drawable.abc_spinner_textfield_background_material,
+        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_edit_text_material)
+    lateinit var s1 : Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +28,10 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
+        s1 = resources.getStringArray(R.array.programming_languages)
+        val adapter = RecyclerAdapter(requireContext(), s1, imgs)
+        view.searchRV.adapter = adapter
+        view.searchRV.layoutManager = LinearLayoutManager(requireContext())
 
         view.textInputLayout.setEndIconOnClickListener{
             if (view.textInputEditText.text.toString().isNotEmpty()) {
